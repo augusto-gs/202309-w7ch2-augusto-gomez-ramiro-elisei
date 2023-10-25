c9da41bfc352aab27904dc7dfdefca524ffd7c07
ramiro elisei <ramiroelisei@gmail.com>
2023-10-25 10:59:29 +0200

    Add CharacterCard test-image

--- src/components/CharacterCard/CharacterCard.test.tsx
+++ src/components/CharacterCard/CharacterCard.test.tsx
@@ -19,4 +19,24 @@ describe("Given a CharacterCard component", () => {
       expect(title).toBeInTheDocument();
     });
   });
+  describe("when it receives the character data and picture url", () => {
+    test("Then it should show the picture of the character", () => {
+      const characterAlt = {
+        name: "Leia",
+        height: "",
+        created: "",
+        mass: "",
+      };
+
+      const imageId = 1;
+
+      const imgUrl = `https://starwars-visualguide.com/assets/img/characters/${imageId}.jpg`;
+
+      render(<CharacterCard image={imageId} character={characterAlt} />);
+
+      const image = screen.getByRole("img");
+      expect(image).toHaveAttribute("src", imgUrl);
+      expect(image).toHaveAttribute("alt", characterAlt.name);
+    });
+  });
 });
