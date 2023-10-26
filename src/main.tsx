7f59c12ce74cad143f570bde13f8f3766aa0120b
ramiro elisei <ramiroelisei@gmail.com>
2023-10-26 10:17:09 +0200

    Add image width-height attribute

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
+import "./components/App/App.css";
 import "../public/styles/styles.css";
 import "./components/App/App.css";
+import "./components/Button/Button.css";
 import "./components/CharacterCard/CharacterCard.css";
 import "./components/CharacterList/CharacterList.css";
-import "./components/Button/Button.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
